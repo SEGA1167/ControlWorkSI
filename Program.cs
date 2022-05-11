665cf9b089ac40c89c80f6a07c6e56d92b59c2c3
Sergei <dr.sega1234@yandex.ru>
2022-05-11 14:54:39 +0300

    создали воид метод для сортировки

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
-﻿void Sorted(string one,string two,string three,string four)
+﻿void printArray(string[] arrayToPrint)
 {
-
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        System.Console.Write(arrayToPrint[i] + ",");
+    }
+    Console.Write("]");
+    System.Console.WriteLine();
 }
 
-Sorted("asvc","1,3","f4","123rs");
+string[] originalArray = { "asvc", "1,3", "f4", "123rs" };
+
+
+int Sorted(string[] originalArray)
+{
+    int count = 0;
+    for (int i = 0; i < originalArray.Length; i++)
+    {
+        if (i <= 3)
+        {
+            count++;
+        }
+    }
+return count;
+}
+
+string[] newArray = new string[count];
+
+
+
